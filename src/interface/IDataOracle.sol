fb9dd39bbe48317b5d3ad3abf8e5e54ce1afeace
tyranis0x01 <tyranis0x01@gmail.com>
2025-09-15 09:03:34 -0700

    feat: Add getData External View Function

--- src/interface/IDataOracle.sol
+++ src/interface/IDataOracle.sol
@@ -5,4 +5,6 @@ pragma solidity ^0.8.19;
  * @title IDataOracle
  * @dev Interface for data oracle contracts
  */
-interface IDataOracle {}
+interface IDataOracle {
+        function getData() external view returns (string memory);
+}
