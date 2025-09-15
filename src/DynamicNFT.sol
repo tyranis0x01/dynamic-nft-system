57e98700ab6c31d185500033dc474d78a0388206
tyranis0x01 <tyranis0x01@gmail.com>
2025-09-15 16:54:19 -0700

    feat: Add Necessary Library Imports

--- src/DynamicNFT.sol
+++ src/DynamicNFT.sol
@@ -1,6 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "./interfaces/IDataOracle.sol";
+import "./interfaces/IMetadataRenderer.sol";
+
 /**
  * @title DynamicNFT
  * @dev NFT contract that changes metadata based on external data sources
