5ec3d8c7096247ee13b8212194160ef420f3eb3e
tyranis0x01 <tyranis0x01@gmail.com>
2025-09-15 09:07:22 -0700

    feat: Add IMetadataRenderer Interface Title Natspec Comments

--- src/interface/IMetadataRenderer.sol
+++ src/interface/IMetadataRenderer.sol
@@ -1,4 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+/**
+ * @title IMetadataRenderer
+ * @dev Interface Of MetadataRender, which generates dynamic metadata and SVG images for NFTs based on their state
+ */
 interface IMetadataRenderer {}
