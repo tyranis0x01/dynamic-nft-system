5c8bf9c006c46ca44b639195fdc1468ad02170b1
tyranis0x01 <tyranis0x01@gmail.com>
2025-09-15 16:53:23 -0700

    feat: Add Contract Title Natspec Comments

--- src/DynamicNFT.sol
+++ src/DynamicNFT.sol
@@ -1,4 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+/**
+ * @title DynamicNFT
+ * @dev NFT contract that changes metadata based on external data sources
+ */
 contract DynamicNFT {}
