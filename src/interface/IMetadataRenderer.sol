a213a0f72a4fc39cd45552b43a448016dabc4986
tyranis0x01 <tyranis0x01@gmail.com>
2025-09-15 09:08:40 -0700

    feat: Add NFTState Struct

--- src/interface/IMetadataRenderer.sol
+++ src/interface/IMetadataRenderer.sol
@@ -5,4 +5,14 @@ pragma solidity ^0.8.19;
  * @title IMetadataRenderer
  * @dev Interface Of MetadataRender, which generates dynamic metadata and SVG images for NFTs based on their state
  */
-interface IMetadataRenderer {}
+interface IMetadataRenderer {
+        struct NFTState {
+        uint256 lastWeatherUpdate;
+        uint256 lastTimeUpdate;
+        uint256 userActionCount;
+        string currentWeather;
+        string currentTimeOfDay;
+        address owner;
+        uint256 createdAt;
+    }
+}
