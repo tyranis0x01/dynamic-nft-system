19b04a9c18e35da3ef4309f47f89e182178150ef
tyranis0x01 <tyranis0x01@gmail.com>
2025-09-17 10:24:20 -0700

    feat: Add NFTUpdated Event

--- src/DynamicNFT.sol
+++ src/DynamicNFT.sol
@@ -39,6 +39,7 @@ contract DynamicNFT is ERC721, Ownable {
 
     // Events
         event NFTMinted(uint256 indexed tokenId, address indexed owner);
+    event NFTUpdated(uint256 indexed tokenId, string updateType, string newValue);
 
     constructor(address _weatherOracle, address _timeOracle, address _metadataRenderer)
         ERC721("Dynamic Weather NFT", "DYNFT")
