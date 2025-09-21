6d78a000072a16967d47733af94b9d3c6bdf6a18
tyranis0x01 <tyranis0x01@gmail.com>
2025-09-20 21:20:40 -0700

    feat: Create & Configure index.tsx file

---
+++ frontend/dynamic-nfts/config/index.tsx
@@ -0,0 +1,22 @@
+import { cookieStorage, createStorage } from "wagmi";
+import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
+import { mainnet, base, optimism } from "@reown/appkit/networks"
+
+export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
+
+if (!projectId) {
+    throw new Error("Project Id is not defined")
+}
+
+export const networks = [mainnet, optimism, base];
+
+export const wagmiAdapter = new WagmiAdapter({
+    storage: createStorage({
+        storage: cookieStorage
+    }),
+    ssr: true,
+    networks,
+    projectId
+});
+
+export const config = wagmiAdapter.wagmiConfig;
