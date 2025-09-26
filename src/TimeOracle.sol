9af550e18aa937937b108ef9a18391f3e91cd5ad
tyranis0x01 <tyranis0x01@gmail.com>
2025-09-26 09:41:00 -0700

    feat: Add timeZones Mapping

--- src/TimeOracle.sol
+++ src/TimeOracle.sol
@@ -8,4 +8,8 @@ import "./interfaces/IDataOracle.sol";
  * @title TimeOracle
  * @dev Oracle contract that provides time-based data for dynamic NFTs
  */
-contract TimeOracle is IDataOracle, Ownable {}
+contract TimeOracle is IDataOracle, Ownable {
+
+   // Time zones mapping (offset in hours from UTC)
+    mapping(string => int256) public timeZones;
+}
